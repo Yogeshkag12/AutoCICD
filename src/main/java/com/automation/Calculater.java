1a42a0c9961183d272709a9d7a7c67254962fc48
Yogeshkag12 <yogikag@gmail.com>
2022-04-07 23:38:44 +0530

    first commit

---
+++ src/main/java/com/automation/Calculater.java
@@ -0,0 +1,20 @@
+package com.automation;
+
+public class Calculater {
+	public int addtion(int x, int y) {
+		return x + y;
+		
+	}
+
+	public int subtraction(int x, int y) {
+		return x - y;
+	}
+
+	public int multipilcation(int x, int y) {
+		return x*y;
+	}
+
+	public int divisio(int x, int y) {
+		return x / y;
+	}
+}
---
+++ src/test/java/com/automation/CalculaterTest.java
@@ -0,0 +1,26 @@
+package com.automation;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CalculaterTest {
+	Calculater cal = new Calculater();
+
+	@Test
+	public void addtion() {
+		Assert.assertEquals(cal.addtion(10, 20), 30);
+	}
+
+	@Test
+	public void subtraction() {
+		Assert.assertEquals(cal.subtraction(20, 10), 10);
+	}
+	@Test
+	public void multilication() {
+		Assert.assertEquals(cal.multipilcation(20, 10), 200);
+	}
+	@Test
+	public void division() {
+		Assert.assertEquals(cal.divisio(20, 10), 2);
+	}
+}
