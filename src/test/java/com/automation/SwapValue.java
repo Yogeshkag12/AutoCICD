f6e60f2bec4dc2cba3dc099aa046c694b0aff8cc
Yogeshkag12 <yogikag@gmail.com>
2022-05-03 14:02:40 +0530

    mondo Create,Update function created

---
+++ src/test/java/com/automation/SwapValue.java
@@ -0,0 +1,15 @@
+package com.automation;
+
+public class SwapValue {
+	
+	public static void main(String[] args) {
+		int x=10;
+		int y=20;
+		x=x+y;
+		y=x-y;
+		x=x-y;
+		System.out.println("Value of x is "+x);
+		System.out.println("Value of y is "+y);
+	}
+
+}
